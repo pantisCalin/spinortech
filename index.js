04a6b98c11f54e03650c6743df9b3fc16027d137
Pantis Calin <92119981+pantisCalin@users.noreply.github.com>
2026-05-01 16:25:42 +0300

    New version
    Only index

--- index.js
+++ index.js
@@ -44,29 +44,30 @@ const CheckOutsideTouch = (e) => {
 };
 
 // Create slider indicators
-const slider_container = document.getElementById("slider_container");
-const slider_parent = slider_container.parentNode;
-const slider_indicator = document.createElement("div");
-slider_indicator.id = "slider_indicator";
-slider_indicator.className = "slider_indicator";
-slider_parent.appendChild(slider_indicator);
-for (let i = 0; i < slider_container.childElementCount; i++) {
-  let dot = document.createElement("div");
-  dot.className = "dot_slider";
-  if (i === 0) {
-    dot.classList.add("active_dot_slider");
-  }
+// const slider_container = document.getElementById("slider_container");
+// const slider_parent = slider_container.parentNode;
+// const slider_indicator = document.createElement("div");
+// slider_indicator.id = "slider_indicator";
+// slider_indicator.className = "slider_indicator";
+// slider_parent.appendChild(slider_indicator);
+// for (let i = 0; i < slider_container.childElementCount; i++) {
+//   let dot = document.createElement("div");
+//   dot.className = "dot_slider";
+//   if (i === 0) {
+//     dot.classList.add("active_dot_slider");
+//   }
 
-  slider_indicator.appendChild(dot);
-}
+//   slider_indicator.appendChild(dot);
+// }
 
-// Swipe functionality for slider
+// const slider_children = slider_container.children;
+// let touchstartX = 0;
+// let touchendX = 0;
+// let currentPage = 0;
+// let lastScrollTop = 0;
 
-// if (!isMobile) {
-//   let touchstartX = 0;
-//   let touchendX = 0;
-//   let currentPage = 0;
-//   // Add touch event listeners to the slider container
+// if (isMobile) {
+//   // Add touch event listeners to the slider container for desktop users, allowing them to swipe using touch input
 //   slider_container.addEventListener(
 //     "touchstart",
 //     (e) => {
@@ -83,129 +84,67 @@ for (let i = 0; i < slider_container.childElementCount; i++) {
 //     },
 //     false,
 //   );
-//   // Handle the swipe gesture to determine the direction and scroll accordingly
-//   const handleGesture = () => {
-//     if (touchendX < touchstartX) {
-//       swipe_right();
-//     } else if (touchendX > touchstartX) {
-//       swipe_left();
-//     } else if (touchendX === touchstartX) {
-//       const middle = pageWidth / 2;
-//       if (touchendX >= middle) {
-//         swipe_right();
-//       } else if (touchendX < middle) {
-//         swipe_left();
+// } else {
+//   // Event listener for the resize
+//   window.addEventListener("resize", () => {
+//     console.log("zoomed");
+//     slider_children[currentPage].scrollIntoView();
+//   });
+
+//   // Wheeling on the page
+//   const wheelPage = (e) => {
+//     if (!e.ctrlKey) {
+//       if (e.deltaY > 0) {
+//         swipe_down();
+//       } else {
+//         swipe_up();
 //       }
 //     }
+//     slider_container.removeEventListener("wheel", wheelPage, false);
+//     setTimeout(() => {
+//       slider_container.addEventListener("wheel", wheelPage, false);
+//     }, 500);
 //   };
+//   slider_container.addEventListener("wheel", wheelPage, false);
 
-//   // Functions to handle swiping left and right, updating the slider indicators and scrolling the slider container
-//   const swipe_left = () => {
-//     slider_indicator.children[currentPage].classList.remove(
-//       "active_dot_slider",
-//     );
-//     currentPage = Math.max(currentPage - 1, 0);
-//     slider_indicator.children[currentPage].classList.add("active_dot_slider");
-//     slider_container.scrollTo(pageWidth * currentPage, 0);
-//   };
-//   const swipe_right = () => {
-//     slider_indicator.children[currentPage].classList.remove(
-//       "active_dot_slider",
-//     );
-//     currentPage = Math.min(
-//       currentPage + 1,
-//       slider_container.childElementCount - 1,
-//     );
-//     slider_indicator.children[currentPage].classList.add("active_dot_slider");
-//     slider_container.scrollTo(pageWidth * currentPage, 0);
-//   };
-// } else {
+//   // Create buttons
+//   const backward_button = document.createElement("button");
+//   const forward_button = document.createElement("button");
 
-const slider_children = slider_container.children;
-let touchstartX = 0;
-let touchendX = 0;
-let currentPage = 0;
-let lastScrollTop = 0;
+//   backward_button.onclick = () => swipe_up();
+//   forward_button.onclick = () => swipe_down();
 
-if (isMobile) {
-  // Add touch event listeners to the slider container for desktop users, allowing them to swipe using touch input
-  slider_container.addEventListener(
-    "touchstart",
-    (e) => {
-      touchstartX = e.changedTouches[0].pageX;
-    },
-    false,
-  );
+//   backward_button.className = "backward_button";
+//   forward_button.className = "forward_button";
 
-  slider_container.addEventListener(
-    "touchend",
-    (e) => {
-      touchendX = e.changedTouches[0].pageX;
-      handleGesture();
-    },
-    false,
-  );
-} else {
-  // Event listener for the resize
-  window.addEventListener("resize", () => {
-    console.log("zoomed");
-    slider_children[currentPage].scrollIntoView();
-  });
+//   slider_parent.appendChild(backward_button);
+//   slider_parent.appendChild(forward_button);
+// }
 
-  // Wheeling on the page
-  const wheelPage = (e) => {
-    if (!e.ctrlKey) {
-      if (e.deltaY > 0) {
-        swipe_down();
-      } else {
-        swipe_up();
-      }
-    }
-    slider_container.removeEventListener("wheel", wheelPage, false);
-    setTimeout(() => {
-      slider_container.addEventListener("wheel", wheelPage, false);
-    }, 500);
-  };
-  slider_container.addEventListener("wheel", wheelPage, false);
+// // Handle the swipe gesture to determine the direction and scroll accordingly
+// const handleGesture = () => {
+//   if (touchendX < touchstartX) {
+//     swipe_down();
+//   } else if (touchendX > touchstartX) {
+//     swipe_up();
+//   }
+// };
 
-  // Create buttons
-  const backward_button = document.createElement("button");
-  const forward_button = document.createElement("button");
+// // Functions to handle swiping left and right, updating the slider indicators and scrolling the slider container
+// const swipe_up = () => {
+//   slider_indicator.children[currentPage].classList.remove("active_dot_slider");
+//   currentPage = Math.max(0, currentPage - 1);
+//   slider_indicator.children[currentPage].classList.add("active_dot_slider");
+//   slider_children[currentPage].scrollIntoView();
+// };
+// const swipe_down = () => {
+//   slider_indicator.children[currentPage].classList.remove("active_dot_slider");
+//   currentPage = Math.min(
+//     currentPage + 1,
+//     slider_container.childElementCount - 1,
+//   );
+//   slider_indicator.children[currentPage].classList.add("active_dot_slider");
+//   slider_children[currentPage].scrollIntoView();
 
-  backward_button.onclick = () => swipe_up();
-  forward_button.onclick = () => swipe_down();
-
-  backward_button.className = "backward_button";
-  forward_button.className = "forward_button";
-
-  slider_parent.appendChild(backward_button);
-  slider_parent.appendChild(forward_button);
-}
-
-// Handle the swipe gesture to determine the direction and scroll accordingly
-const handleGesture = () => {
-  if (touchendX < touchstartX) {
-    swipe_down();
-  } else if (touchendX > touchstartX) {
-    swipe_up();
-  }
-};
-
-// Functions to handle swiping left and right, updating the slider indicators and scrolling the slider container
-const swipe_up = () => {
-  slider_indicator.children[currentPage].classList.remove("active_dot_slider");
-  currentPage = Math.max(0, currentPage - 1);
-  slider_indicator.children[currentPage].classList.add("active_dot_slider");
-  slider_children[currentPage].scrollIntoView();
-};
-const swipe_down = () => {
-  slider_indicator.children[currentPage].classList.remove("active_dot_slider");
-  currentPage = Math.min(
-    currentPage + 1,
-    slider_container.childElementCount - 1,
-  );
-  slider_indicator.children[currentPage].classList.add("active_dot_slider");
-  slider_children[currentPage].scrollIntoView();
-
-  // slider_container.scrollTo(0, slider_position);
-};
+//   // slider_container.scrollTo(0, slider_position);
+// };
